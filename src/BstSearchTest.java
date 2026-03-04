162ff156539892d286e0e291c9b6b3ce922f9a87
Lilian <210286008+liliann19@users.noreply.github.com>
2026-03-04 00:12:55 -0800

    implemented testContainsValueInTree

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+    @Test
+    public void testContainsValueInTree() {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(5, null, null);
+        root.left = new BinaryTreeNode<Integer>(3, null, null);
+        root.right = new BinaryTreeNode<Integer>(9, null, null);
+
+        assertTrue(BstSearch.contains(root, 9));
+    }
 
 }
